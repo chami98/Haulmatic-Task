f602815eba5326dcb10eff8a819fb1dbe194b2b5
chami98 <chamikara98@gmail.com>
2024-04-05 22:37:35 +0530

    Implemented CreateUserTile component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import AppBar from '../components/AppBar'
+import CreateUserTile from '../components/CreateUserTile'
 
 export default function Home() {
     return (
         <>
             <AppBar />
+
+            <CreateUserTile />
         </>
     )
 }
