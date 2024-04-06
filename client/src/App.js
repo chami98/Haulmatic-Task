89af25cea3a6c9c1740e4bf5888538afd90e3b5e
chami98 <chamikara98@gmail.com>
2024-04-06 13:36:34 +0530

    Get isAuthenticated state from redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import Initialize from './pages/Initialize';
+import { useSelector } from 'react-redux';
+
 
 function App() {
 
-  const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('isAuthenticated') === 'true');
-  const [isInitialized, setIsInitialized] = useState(false)
+  const { isInitialized } = useSelector(state => state.app)
+  console.log(isInitialized)
+  const { isAuthenticated } = useSelector(state => state.app)
+
+
 
   let routes = null;
 
@@ -17,8 +22,9 @@ function App() {
     routes = <Route path="*" element={<Initialize />} />
   } else {
     routes = <>
-      <Route exact path="/signin" element={!isAuthenticated ? <SignIn setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/" />} />
+      <Route exact path="/signin" element={!isAuthenticated ? <SignIn /> : <Navigate to="/" />} />
       <Route exact path="/" element={isAuthenticated ? <Home /> : <Navigate to="/signin" />} />
+      {/* <Route path="*" element={<Navigate to="/signin" />} /> */}
     </>
   }
 
