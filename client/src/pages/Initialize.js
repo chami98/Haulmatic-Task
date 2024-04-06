4a7e14c9805363e03277978c100a8c9f2497894b
chami98 <chamikara98@gmail.com>
2024-04-06 13:29:00 +0530

    Create Initialize component

--- client/src/pages/Initialize.js
+++ client/src/pages/Initialize.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAppInitializedState } from '../redux/actions/appActions';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
 export default function Initialize() {
+
+    const { isInitialized } = useSelector(state => state.app)
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(setAppInitializedState(true))
+        console.log("dispatching")
+    }, [])
+
+
+    console.log(isInitialized)
+
+    const [loading, setLoading] = useState(true)
+
     return (
-        <div>
-            loading
-        </div>
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={loading}
+            onClick={() => setLoading(false)}
+        >
+            <CircularProgress color="inherit" />
+            <Typography variant="h6" color="inherit" component="div" sx={{ ml: 2 }}>
+                Signing in...
+            </Typography>
+        </Backdrop>
     )
 }
