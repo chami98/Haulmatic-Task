914e2516b9471b50c18d6e0886e1747359110ac3
chami98 <chamikara98@gmail.com>
2024-04-07 02:59:02 +0530

    Add firebase hosting and cloud functions

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,5 @@
+let firebaseFunctionsURL = 'https://us-central1-haulmatic-task.cloudfunctions.net/haulmaticTask'
+let localURL = 'http://localhost:5000'
+
+export const BASE_URL = localURL
 export const TOKEN_KEY = 'TOKEN_KEY'
-export const BASE_URL = 'http://localhost:5000'
